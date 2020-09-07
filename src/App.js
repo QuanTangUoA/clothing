69e1468e150e962a7e04f08abd83b3bb6833d98d
QuanTang <quantanguoa@gmail.com>
2020-09-07 18:31:49 +0930

    update routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
-
+import {Switch, Route, Link} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = props =>{
+  return (
+    <div>
+      <Link to='/topics'>Topics</Link>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
