9e8c683dc895618891f25452a64d9aa36ccdf9cd
QuanTang <quantanguoa@gmail.com>
2021-01-14 19:45:42 +1030

    implemented signup but the authentication part has bugs which will be fixed later

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,6 +14,28 @@ const QuansConfig = {
 
 firebase.initializeApp(QuansConfig);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapshot = await userRef.get();
+    if(!snapshot){
+        const {displayName, email} = userAuth;
+        console.log('AAA');
+        console.log('BBB');
+        const createdAt = new Date();
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        }catch (err) {
+            console.log('Failed to create new user, ',err)
+        };
+    }
+    return userRef;
+}
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 const provider = new firebase.auth.GoogleAuthProvider();
